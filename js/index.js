eeb2dc26e297c896e6e337a8bed2d01a644a62e9
Mickey <mickeyc11111@gmail.com>
2022-06-05 15:19:04 -0400

    removed console.logs and added users to user list

--- js/index.js
+++ js/index.js
@@ -7,29 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch(`https://api.github.com/search/users?q=${input}`)
         .then(res => res.json())
         .then(data => {
-            let objList = data.items
-            console.log(data)
-            console.log(data.items)
             for(let i = 0; i < data.items.length; i++) {
                 console.log(data.items[i].login)
                 createUser(data.items[i].login)
-            }
-            
+            }  
         })
         })
     })
 
-
-
-    // {
-    //     for(const user in data.items) {
-    //         let li = document.createElement('li');
-    //         li.textContent = user.login;
-    //         document.getElementById('github-container').appendChild(li);
-    //     }
-
     function createUser (user) {
         let ol = document.createElement('ol');
         ol.textContent = user;
-        document.getElementById('github-container').appendChild(ol)
+        document.getElementById('user-list').appendChild(ol)
     }
